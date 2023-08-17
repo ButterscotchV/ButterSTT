74d1502996fd0c41362d2207b1f628035cfacdaa
Butterscotch! <bscotchvanilla@gmail.com>
2023-08-17 18:24:36 -0400

    Add basic real-time transcription to the console

--- ButterSTT/Program.cs
+++ ButterSTT/Program.cs
@@ -1,2 +1,54 @@
-// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+using AprilAsr;
+using NAudio.Wave;
+
+var modelPath = "C:\\Users\\Butterscotch\\Downloads\\april-english-dev-01110_en.april";
+
+// Load the model and print metadata
+var model = new AprilModel(modelPath);
+Console.WriteLine("Name: " + model.Name);
+Console.WriteLine("Description: " + model.Description);
+Console.WriteLine("Language: " + model.Language);
+
+var session = new AprilSession(model, (result, tokens) =>
+{
+    string s = "";
+    if (result == AprilResultKind.PartialRecognition)
+    {
+        s = "- ";
+    }
+    else if (result == AprilResultKind.FinalRecognition)
+    {
+        s = "@ ";
+    }
+    else
+    {
+        s = " ";
+    }
+
+    foreach (AprilToken token in tokens)
+    {
+        s += token.Token;
+    }
+
+    Console.WriteLine(s);
+}, async: true);
+
+using var recorder = new WaveInEvent
+{
+    WaveFormat = new WaveFormat(16000, 16, 1)
+};
+
+recorder.DataAvailable += (sender, args) =>
+{
+    if (args.BytesRecorded <= 0) return;
+    var shorts = new short[args.BytesRecorded / 2];
+    Buffer.BlockCopy(args.Buffer, 0, shorts, 0, args.BytesRecorded);
+    session.FeedPCM16(shorts, shorts.Length);
+};
+recorder.RecordingStopped += (sender, args) =>
+{
+    session.Flush();
+};
+
+recorder.StartRecording();
+Console.ReadLine();
