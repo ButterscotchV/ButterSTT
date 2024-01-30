c2db75643b1975af228e0da4074e6a6f40ebca74
Butterscotch! <bscotchvanilla@gmail.com>
2024-01-30 03:26:08 -0500

    Handle exception on the main program better

--- ButterSTT/Program.cs
+++ ButterSTT/Program.cs
@@ -2,60 +2,69 @@ using ButterSTT;
 using ButterSTT.Config;
 using ButterSTT.MessageSystem;
 
-// Load config
-JsonConfigHandler<STTConfig> configHandler =
-    new("config.json", JsonConfigHandler<STTConfig>.Context.STTConfig);
-STTConfig config = configHandler.InitializeConfig(STTConfig.Default);
-
-// Fill in new values if the version changed
-if (config.ConfigVersion < STTConfig.Default.ConfigVersion)
+try
 {
-    var backupFile = $"{configHandler.ConfigFilePath}.old";
-    configHandler.MakeBackup(backupFile);
-    Console.WriteLine(
-        $"Backed up the current config file to \"{backupFile}\", upgrading from v{config.ConfigVersion} to v{STTConfig.Default.ConfigVersion}..."
+    // Load config
+    JsonConfigHandler<STTConfig> configHandler =
+        new("config.json", JsonConfigHandler<STTConfig>.Context.STTConfig);
+    STTConfig config = configHandler.InitializeConfig(STTConfig.Default);
+
+    // Fill in new values if the version changed
+    if (config.ConfigVersion < STTConfig.Default.ConfigVersion)
+    {
+        var backupFile = $"{configHandler.ConfigFilePath}.old";
+        configHandler.MakeBackup(backupFile);
+        Console.WriteLine(
+            $"Backed up the current config file to \"{backupFile}\", upgrading from v{config.ConfigVersion} to v{STTConfig.Default.ConfigVersion}..."
+        );
+        config.ConfigVersion = STTConfig.Default.ConfigVersion;
+        configHandler.WriteConfig(config);
+    }
+
+    // Setup model dir
+    var modelsDir = new DirectoryInfo(config.ModelsPath);
+    modelsDir.Create();
+
+    // Find the first model available
+    var modelFile =
+        modelsDir
+            .GetFiles()
+            .FirstOrDefault(
+                file =>
+                    file != null
+                    && file.Extension.Equals(".april", StringComparison.CurrentCultureIgnoreCase),
+                null
+            )
+        ?? throw new FileNotFoundException(
+            $"Could not find any available AprilAsr models (*.april) in \"{modelsDir}\"."
+        );
+
+    var messageQueue = new MessageQueue()
+    {
+        MessageLength = config.MessageLength,
+        MaxWordsDequeued = config.MaxWordsDequeued < 0 ? int.MaxValue : config.MaxWordsDequeued,
+        RealtimeQueuePadding = config.RealtimeQueuePadding,
+        WordTime = config.WordTime,
+        HardWordTime = config.HardWordTime,
+    };
+    using var oscHandler = new OSCMessageHandler(messageQueue, config.OSCEndpoint)
+    {
+        RateLimit = config.OSCChatboxRateLimit
+    };
+    oscHandler.StartMessageLoop();
+
+    using var speechToTextHandler = new SpeechToTextHandler(
+        modelFile,
+        oscHandler.MessageQueue,
+        deviceNumber: config.MicrophoneDeviceNumber
     );
-    config.ConfigVersion = STTConfig.Default.ConfigVersion;
-    configHandler.WriteConfig(config);
+    speechToTextHandler.StartRecording();
+
+    Console.ReadLine();
 }
-
-// Setup model dir
-var modelsDir = new DirectoryInfo(config.ModelsPath);
-modelsDir.Create();
-
-// Find the first model available
-var modelFile =
-    modelsDir
-        .GetFiles()
-        .FirstOrDefault(
-            file =>
-                file != null
-                && file.Extension.Equals(".april", StringComparison.CurrentCultureIgnoreCase),
-            null
-        )
-    ?? throw new FileNotFoundException(
-        $"Could not find any available AprilAsr models (*.april) in \"{modelsDir}\"."
-    );
-
-var messageQueue = new MessageQueue()
+catch (Exception e)
 {
-    MessageLength = config.MessageLength,
-    MaxWordsDequeued = config.MaxWordsDequeued < 0 ? int.MaxValue : config.MaxWordsDequeued,
-    RealtimeQueuePadding = config.RealtimeQueuePadding,
-    WordTime = config.WordTime,
-    HardWordTime = config.HardWordTime,
-};
-using var oscHandler = new OSCMessageHandler(messageQueue, config.OSCEndpoint)
-{
-    RateLimit = config.OSCChatboxRateLimit
-};
-oscHandler.StartMessageLoop();
-
-using var speechToTextHandler = new SpeechToTextHandler(
-    modelFile,
-    oscHandler.MessageQueue,
-    deviceNumber: config.MicrophoneDeviceNumber
-);
-speechToTextHandler.StartRecording();
-
-Console.ReadLine();
+    Console.Error.WriteLine(e);
+    Console.WriteLine("Press any key to exit...");
+    Console.ReadKey();
+}
